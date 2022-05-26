59da2a009a42cf2b99d2c3fca9e9abbbb46043aa
유병찬 <chanyb@yubyeongchan-ui-iMac.local>
2022-05-26 22:32:51 +0900

    update: blog 카드 디자인 업데이트

--- src/components/blog.vue
+++ src/components/blog.vue
@@ -1,10 +1,17 @@
 <template>
 	<div>
-		<div class="row">
-			<div class="col-md-12" style="text-align:center;">
-				<ul>
-					<li>this is first contents</li>
-				</ul>
+		<div class="row justify-content-center align-items-center" style="min-height:100px;">
+			<div class="col-md-12 text-center">blog</div>
+		</div>
+		<div class="row justify-content-center" style="margin-bottom:10px;">
+			<div class="col-md-4">
+				<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">Special title treatment</h5>
+					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -13,81 +20,11 @@
 <script>
 export default {
 	name:"componentBlog",
-	props: {
-		msg: String,
-	},
-	methods: {
-		moveLeft: function () {
-			var mainFrame = document.getElementById("main");
-			mainFrame.classList.add("moveLeft");
-		},
-		toggleItem: function (val) {
-			var skillBody = document.getElementById("skillBody");
-
-			var targetClass = skillBody.classList;
-
-			if (targetClass.length === 1) {
-				skillBody.classList.add("col-md-6");
-				skillBody.classList.add("moveLeft");
-				skillBody.classList.remove("col-md-12");
-
-				skillBody.addEventListener(
-					"animationend",
-					() => {
-						this.flag.itemToggle = val;
-					},
-					false,
-				);
-			} else {
-				this.flag.itemToggle = val;
-			}
-		},
-	},
-	data() {
-		return {
-			flag: {
-				itemToggle: false,
-			},
-		};
-	},
+	
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.item {
-	margin-right: 10px;
-}
-.item:hover {
-	color: #238038;
-	cursor: pointer;
-}
-.preparing {
-	color: gray;
-}
-.preparing:hover {
-	color: gray;
-	cursor: default;
-}
 
-@-webkit-keyframes moveLeft {
-	from {
-		left: 30vh;
-	}
-	to {
-		left: 0px;
-	}
-}
-
-.moveLeft {
-	animation-name: moveLeft;
-	animation-duration: 1s;
-	animation-fill-mode: forwards;
-}
-
-@media all and (min-width: 1200px) {
-	.window-mr {
-		padding-right: 10%;
-	}
-}
 </style>
