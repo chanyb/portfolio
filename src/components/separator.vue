570f1c3b7efa34ff2ef555fb37a7b0b62a75add8
유병찬 <chanyb@yubyeongchan-ui-iMac.local>
2022-05-27 22:52:27 +0900

    update: add margin-top 5% in separator

--- src/components/separator.vue
+++ src/components/separator.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<div class="row">
-			<div class="col-md-12" style="background-color:gray; min-height: 10px;">
+			<div class="col-md-12" style="background-color:gray; min-height: 10px; margin-top:5%;">
 			</div>
 		</div>
 	</div>
