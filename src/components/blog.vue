39c3fb330a15c517818c1744609e495e7fc9b446
unknown <chanyb0827@gmail.com>
2022-05-30 13:28:18 +0900

    update: (blog) 게시물 수정, 버튼 수정

--- src/components/blog.vue
+++ src/components/blog.vue
@@ -5,7 +5,7 @@
 		</div>
 		<div class="row justify-content-center" style="margin-bottom:10px;">
 			<div class="col-md-4">
-				<div class="card" style="width: 300px; margin:auto">
+				<div class="card" style="width: 300px; margin: auto;">
 					<div style="position:absolute; width:100%; top:-20px;">
 						<span>2022-05-27 23:20</span>
 					</div>
@@ -15,7 +15,24 @@
 						<p class="card-text" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">사실 시작은 4월에 전주에서부터 시작했지만, 제대로 강의를 수강한 것은 이직하고 부터다</p>
 						<div class="row">
 							<div class="col-md-12">
-								<a href="#" class="btn btn-primary" style="width:100%;" @click="prepare()">Go somewhere</a>
+								<button href="" class="btn btn-primary" style="width:100%;" @click="prepare()">바로가기</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card" style="width: 300px; margin:auto">
+					<div style="position:absolute; width:100%; top:-20px;">
+						<span>2022-05-27 23:20</span>
+					</div>
+					<img src="https://images.velog.io/images/jojo_devstory/post/dae32386-bffc-40c3-b866-5c1e64516902/Android%2010_0.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title" style="font-size:20px;">안드로이드 생명주기</h5>
+						<p class="card-text" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">안드로이드 생명주기는 Activity의 생명주기와 Fragment 생명주기, Service 생명주기 등 다양하게 존재한다. 현재 내가 또다른 어떤 생명주기를 모르고 있는지를 모른다.. </p>
+						<div class="row">
+							<div class="col-md-12">
+								<button href="" class="btn btn-primary" style="width:100%;" @click="prepare()">바로가기</button>
 							</div>
 						</div>
 					</div>
